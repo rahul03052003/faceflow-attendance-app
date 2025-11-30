b2cbf2c5a18af6780bd515f4cd4fa510597d373f
Rahul V <rv03052003@gmail.com>
2025-11-30 22:21:41 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 // This file is automatically populated by Firebase Studio.
 // Do not edit manually.
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyCkIwn3-Y4Ok9ubeTPDuPYQLP7pShs3sKA",
+  authDomain: "studio-8681400064-b9527.firebaseapp.com",
+  projectId: "studio-8681400064-b9527",
+  storageBucket: "studio-8681400064-b9527.appspot.com",
+  messagingSenderId: "1082156488123",
+  appId: "1:1082156488123:web:4b99a9658e6665eef3209e",
 };
