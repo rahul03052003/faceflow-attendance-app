e86b2ebc0f5548aad11febc9a9faf54a786eaa89
Rahul V <rv03052003@gmail.com>
2025-12-01 09:45:24 +0000

    why everytime i have to add new subject and user it is not permanently s

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,12 +1,25 @@
-import { initializeApp } from 'firebase/app';
+
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { firebaseConfig } from './config';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 function initializeFirebase() {
-  const app = initializeApp(firebaseConfig);
+  const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const firestore = getFirestore(app);
+
+  // NOTE: The conditional statement below is for connecting to the
+  // local Firebase Emulator Suite. You can remove this block if you
+  // are not using the emulators.
+  if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined' && !auth.emulatorConfig) {
+    // Point to the emulators.
+    // This is required for local development.
+    // connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+    // connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+  }
+
+
   return { app, auth, firestore };
 }
 
