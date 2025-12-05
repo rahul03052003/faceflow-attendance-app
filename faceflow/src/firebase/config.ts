1c1e919a9245a23faa6779c27bafc2fa29c5db44
Rahul V <rv03052003@gmail.com>
2025-12-05 21:55:44 +0000

    what is the api key of my porject

---
+++ faceflow/src/firebase/config.ts
@@ -0,0 +1,12 @@
+
+// This file is automatically populated by Firebase Studio.
+// Do not edit manually.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkIwn3-Y4Ok9ubeTPDuPYQLP7pShs3sKA",
+  authDomain: "studio-8681400064-b9527.firebaseapp.com",
+  projectId: "studio-8681400064-b9527",
+  storageBucket: "studio-8681400064-b9527.appspot.com",
+  messagingSenderId: "1082156488123",
+  appId: "1:1082156488123:web:4b99a9658e6665eef3209e",
+};
+
